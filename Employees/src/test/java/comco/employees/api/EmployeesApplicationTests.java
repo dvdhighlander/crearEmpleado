2799c400a47b5fea3e9756de6658a4e383ab9a9e
dvdhighlander <61250900+dvdhighlander@users.noreply.github.com>
2025-06-12 16:52:43 -0500

    tests
    Se añade test de conexion

--- Employees/src/test/java/comco/employees/api/EmployeesApplicationTests.java
+++ Employees/src/test/java/comco/employees/api/EmployeesApplicationTests.java
@@ -4,10 +4,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class EmployeesApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+    
 	@Test
-	void contextLoads() {
-	}
+	public void successIfServiceResponseOK() throws Exception {
 
+	    mockMvc.perform(MockMvcRequestBuilders.get("/employee/create")
+	            .param("nombre", "testName")
+	            .param("apellido", "testApellido")
+	            .param("tipoDocumento", "CC")
+	            .param("numeroDocumento", "1098765432")
+	            .param("fechaNacimiento", "2000-01-21")
+	            .param("fechaVinculacion", "2025-01-03")
+	            .param("cargo", "testCargo")
+	            .param("salario", "123456")
+	            )
+	            .andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
